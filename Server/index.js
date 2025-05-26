9475d175510e05bdc48970e94a7e2f55fd8efc7e
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-05-27 02:35:32 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -21,6 +21,13 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true                 // 👈 this is REQUIRED to allow cookies
 }));
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true, // very important for SameSite=None
+  sameSite: "None", // fixes cross-site issue
+  maxAge: 24 * 60 * 60 * 1000, // 1 day
+});
+
 
 
 // res.cookie('token', token, {
