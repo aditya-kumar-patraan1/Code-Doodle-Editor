8100ca52dbe67fa74c4daa9fc32907453cf72366
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-05-27 22:15:12 +0530

    Update myControllers.js

--- Server/Controllers/myControllers.js
+++ Server/Controllers/myControllers.js
@@ -140,10 +140,11 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    });
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+});
+
 
     return res.status(200).send({
       status: 1,
