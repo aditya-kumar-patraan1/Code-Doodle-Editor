2abff298af81e5a13206a517a408944072f7e536
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-10-24 18:51:14 +0530

    Changing the Gemini Latest Model for Chatbot in codedoodle

--- Client/src/ChatBox/gemini.js
+++ Client/src/ChatBox/gemini.js
@@ -1,28 +1,44 @@
-import {GoogleGenerativeAI} from '@google/generative-ai';
+// import {GoogleGenerativeAI} from '@google/generative-ai';
 
 
-const apiKey = import.meta.env.VITE_GEMINI_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
+// const apiKey = "AIzaSyCWfWJ7Ne25n3NdxHCEOn92qlJUXQr6BIY";
+// const genAI = new GoogleGenerativeAI(apiKey);
 
-async function generateContent(prompt){
-    try{
-        const model = await genAI.getGenerativeModel({model : 'gemini-1.5-flash'});
+// async function generateContent(prompt){
+//     try{
+//         const model = await genAI.getGenerativeModel({model : 'gemini-1.5-flash'});
 
-        const chatSession = model.startChat({
-            generationConfig : {
-                temperature : 1,
-                topP : 0.95,
-                topK : 64,
-                maxOutputTokens : 8192,
-            },
-            history : [],
-        });
+//         const chatSession = model.startChat({
+//             generationConfig : {
+//                 temperature : 1,
+//                 topP : 0.95,
+//                 topK : 64,
+//                 maxOutputTokens : 8192,
+//             },
+//             history : [],
+//         });
 
-        const result = await chatSession.sendMessage(prompt);
-        return result.response.text();
-        }catch(error){
-            throw new Error("This is my Error !");
-        }
+//         const result = await chatSession.sendMessage(prompt);
+//         return result.response.text();
+//         }catch(error){
+//             throw new Error("This is my Error !");
+//         }
+// }
+
+// export default generateContent;
+import { GoogleGenAI } from "@google/genai";
+
+const API_KEY=import.meta.env.VITE_GEMINI_API_KEY;
+
+const ai = new GoogleGenAI({ apiKey: API_KEY});
+
+async function generateContent(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+  console.log(response.text);
+  return response.text;
 }
 
 export default generateContent;
