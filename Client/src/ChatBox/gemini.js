f2e7e72e3b791b31043e9ec8cfb64109790a4fdc
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-10-24 19:47:29 +0530

    added hardcoded api key

--- Client/src/ChatBox/gemini.js
+++ Client/src/ChatBox/gemini.js
@@ -28,7 +28,7 @@
 // export default generateContent;
 import { GoogleGenAI } from "@google/genai";
 
-const API_KEY=import.meta.env.VITE_GEMINI_API_KEY;
+const API_KEY="AIzaSyBkljrv58uEQagUlfqNW8s7jbtmUSfwekw";
 
 const ai = new GoogleGenAI({ apiKey: API_KEY});
 
