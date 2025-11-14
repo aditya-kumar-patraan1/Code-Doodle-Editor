365c16423f3b12f142ed57a114d136abedeb141d
Aditya Kumar Rajput <pythonboot06@gmail.com>
2025-11-14 21:59:29 +0530

    Add addFileToRecycleBin to fileRoutes

--- Server/Routings/fileRoutes.js
+++ Server/Routings/fileRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router4 = express.Router();
 const userAuth = require('../MiddleWares/userAuth');
-const {addFile, deleteFile, deleteOneFile} = require('../Controllers/folderController');
+const {addFile, deleteFile, deleteOneFile,addFileToRecycleBin} = require('../Controllers/folderController');
 
 router4.post('/addFile',userAuth, addFile);
 router4.post('/deleteFile',userAuth, deleteFile);
