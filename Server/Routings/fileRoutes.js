376fb15b145cbd204100e2a6a9f15bf4457969c3
Aditya Kumar Rajput <pythonboot06@gmail.com>
2026-01-08 16:02:09 +0530

    Add deleteAllRecycleBinFile route to fileRoutes

--- Server/Routings/fileRoutes.js
+++ Server/Routings/fileRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router4 = express.Router();
 const userAuth = require('../MiddleWares/userAuth');
-const {addFile, deleteFile, deleteOneFile,addFileToRecycleBin,getRecycleBinFiles} = require('../Controllers/folderController');
+const {addFile, deleteFile, deleteOneFile,addFileToRecycleBin,getRecycleBinFiles,deleteAllRecycleBinFile} = require('../Controllers/folderController');
 
 router4.post('/addFile',userAuth, addFile);
 router4.post('/deleteFile',userAuth, deleteFile);
@@ -9,4 +9,5 @@ router4.post("/deleteOneFile",userAuth,deleteOneFile);
 //added for recycle din
 router4.post("/addToRecycleBin",userAuth,addFileToRecycleBin);
 router4.post("/getRecycleBinFiles",userAuth,getRecycleBinFiles);
+router4.delete("/deleteAllRecycleBinFiles",userAuth,deleteAllRecycleBinFile)
 module.exports = router4;
